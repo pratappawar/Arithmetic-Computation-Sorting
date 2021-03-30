77757bab2098475fd2876386f7a453a3183d16ee
PratapPawar <pratappawar1997@gmail.com>
2021-03-30 23:38:18 +0530

    Added: Airthmetic Operation--> c+a/b

--- Computation_sorting.sh
+++ Computation_sorting.sh
@@ -4,7 +4,7 @@ read -p "Enter first number: " a
 read -p "Enter second number: " b
 read -p "Enter third number: " c
 echo $a  $b  $c
-#COMPUTE ARITHMETIC OPERATION_2
-result=$(($a * $b + $c))
+#COMPUTE ARITHMETIC OPERATION_3
+result=$(($c + ($a/$b)))
 echo "The Result= " $result
 
