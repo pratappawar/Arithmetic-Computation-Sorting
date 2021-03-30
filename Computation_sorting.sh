2e272a1acda1605845ef7dad93da917d0b888eee
PratapPawar <pratappawar1997@gmail.com>
2021-03-31 00:56:29 +0530

    Added: Store the Result In Dictionary

--- Computation_sorting.sh
+++ Computation_sorting.sh
@@ -1,10 +1,18 @@
 #! /bin/bash
-
+#TO DECLEAR THE DICTIONARY
+declare -A ArithmaticOperation
 read -p "Enter first number: " a
 read -p "Enter second number: " b
 read -p "Enter third number: " c
 echo $a  $b  $c
 #COMPUTE ARITHMETIC OPERATION_3
-result=$((($a%$b)+$c))
-echo "The Result= " $result
+result1=$(($a+$b*$c))
+result2=$(($a*$b+$c))
+result3=$(($c + ($a/$b)))
+result4=$((($a%$b)+$c))
+#To Store the result in dictionary
+ArithmaticOperation[result1]=$result1
+ArithmaticOperation[result2]=$result2
+ArithmaticOperation[result3]=$result3
+ArithmaticOperation[result4]=$result4
 
